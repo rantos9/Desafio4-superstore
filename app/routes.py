cc95094873d7f4c18ee72b340948a5946c0b9f6d
Silvio Luiz <silvio.luiz@gmail.com>
2019-05-24 01:35:35 -0300

    Alteracao dos parametros de entrada da API

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,32 @@
+from flask import request, jsonify, abort, make_response
 from app import app
-from flask import request, jsonify, abort, make_response, Response
-from datetime import datetime
 
-@app.route('/predict/time_series/<algorithim>', methods=['GET'])
-def predict_by_category(algorithim):
+@app.route('/timeseries/predict', methods=['GET'])
+def timeseries_predict():
     '''
-    Metodo que executa a predicao do time_series, de acordo com algoritmo selecionado.
-    Algorithim:
-        ARIMA, SARIMAX, etc...
-    Parâmetros URL Get:
-        categoria
-        data
+    Metodo que executa a predicao do time_series, de acordo com os parâmetros selecionados.
+    algorithm:
+        ARIMA, SARIMAX
+    category:
+        'Office Supplies', 'Furniture', 'Technology'
+    cycles:
+        Número de janelas a partir de 12/2017
     '''
 
     #Carregar lista de categorias da planilha
-    category_list = ['cat1', 'cat2', 'cat3']
+    category_list = ['Office Supplies', 'Furniture', 'Technology']
+    algorithm_list = ['ARIMA', 'SARIMAX']
 
-    param_date = get_date_parameter('date')
-    param_category = request.args.get('category')
+    param_algorithm = request.args.get('algorithm').strip('\'"')
+    param_cycles = request.args.get('cycles', type=int)
+    param_category = request.args.get('category').strip('\'"')
+    
+    if param_algorithm.upper() not in algorithm_list:
+        abort(make_response(jsonify(message='Algorithm is not valid'), 422))
+
     if param_category not in category_list:
         abort(make_response(jsonify(message='Category is not valid'), 422))
 
     #Carregar Modelo e passar algorithm, category e date como parametros
-    return  jsonify(f'Category: {param_category}, Data: {param_date}, Algorithm:{algorithim}'), 200
-
-def get_date_parameter(name):
-    try:
-        #Procurar por uma solucao flask pra substituir o strip
-        date_parameter = request.args.get(name).strip('\'"')
-        return datetime.strptime(date_parameter, '%d/%m/%Y').date()
-    except ValueError as v_error:
-        print(v_error)
-        abort(make_response(jsonify(message='Date is not valid'), 422))
+    return  jsonify(f'Category: {param_category}, Cycles: {param_cycles}, Algorithm:{param_algorithm}'), 200
     
