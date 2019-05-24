6eb75b6c4d20f038d451a10b550bd1409f460a90
Silvio Luiz <silvio.luiz@gmail.com>
2019-05-24 01:56:40 -0300

    Inclusao de rotas pra listar categorias e produtos por categoria

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import request, jsonify, abort, make_response
 from app import app
 
-@app.route('/timeseries/predict', methods=['GET'])
+@app.route('/superstore/predict', methods=['GET'])
 def timeseries_predict():
     '''
     Metodo que executa a predicao do time_series, de acordo com os parâmetros selecionados.
@@ -20,7 +20,7 @@ def timeseries_predict():
     param_algorithm = request.args.get('algorithm').strip('\'"')
     param_cycles = request.args.get('cycles', type=int)
     param_category = request.args.get('category').strip('\'"')
-    
+   
     if param_algorithm.upper() not in algorithm_list:
         abort(make_response(jsonify(message='Algorithm is not valid'), 422))
 
@@ -28,5 +28,19 @@ def timeseries_predict():
         abort(make_response(jsonify(message='Category is not valid'), 422))
 
     #Carregar Modelo e passar algorithm, category e date como parametros
-    return  jsonify(f'Category: {param_category}, Cycles: {param_cycles}, Algorithm:{param_algorithm}'), 200
+    return jsonify(f'Cat: {param_category},Cycles: {param_cycles},Algorithm:{param_algorithm}'), 200
+
+@app.route('/superstore/categories/<category_id>/products')
+def products_by_category(category_id):
+    '''
+    Obtém a lista de códigos de produto por categoria
+    '''
+    return f'products_by_category {category_id}', 200
+
+@app.route('/superstore/categories')
+def categories():
+    '''
+    Obtém a lista de categorias
+    '''
+    return 'categories', 200
     
